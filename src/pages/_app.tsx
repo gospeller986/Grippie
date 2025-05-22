56806806946e18dbabe163e8668fcc32233d4aca
satyajit pal <palsatyajit986@gmail.com>
2025-05-22 20:57:32 +0530

    animations for offer slider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "@/components/ui/sonner"
 export default function App({ Component, pageProps, router }: AppProps) {
      return (
           <div>
+               <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
                <AnimatePresence mode="wait" >
                     <main className={`${fontPrometoTrial.variable} ${fontProximaNovaAlt.variable}`}>
                          <Component key={router.route} {...pageProps} />
