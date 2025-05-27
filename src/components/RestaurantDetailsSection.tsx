27d5d736b3cc04cece4099c79968c283d23c93a2
satyajit pal <palsatyajit986@gmail.com>
2025-05-27 09:08:38 +0530

    font size changes

--- src/components/RestaurantDetailsSection.tsx
+++ src/components/RestaurantDetailsSection.tsx
@@ -8,8 +8,8 @@ const RestaurantDetailsSection = () => {
     }
     return (
         <>
-            <h1 className='md:text-[40px] text-[25px]'
-             style={{ fontWeight: "bold", paddingLeft: "18px", paddingRight: "18px", fontFamily: "Prometo", lineHeight: "12px" }}>Play Qutub</h1>
+            <h1 className='md:text-[40px] text-[28px]'
+             style={{ fontWeight: "700 !important", paddingLeft: "18px", paddingRight: "18px", fontFamily: "Prometo", lineHeight: "12px" }}>Play Qutub</h1>
             <p className='mt-[16px] md:mt-[22px]' style={{ paddingLeft: "18px", paddingRight: "18px", marginTop: '16px', fontWeight: "600" , fontFamily : 'ProximaNovaL' , fontSize : '15px' }}>
                 <span style={{ color: "#8CBB41" }}>OPEN</span>
                 <span style={{ color: "#5A5A5A" }}> ·   12PM - 1AM   |   Qutub Institutional Area </span>
